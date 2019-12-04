cba58ac37fe560418464d4e3e89f8b70e4f8859f
Ryan Mckenna <mckenna1995@gmail.com>
2019-12-04 17:45:29 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-navbar toggleable="md" variant="dark" type="dark">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <img src="./assets/movielove.png" class="img-circle" style="padding: 5px">
+      <b-navbar-brand to="/">Movie lovers Web App</b-navbar-brand>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item to="/#"><i class="fa fa-home" style="padding: 5px"> Home</i></b-nav-item>
+          <b-nav-item to="/movies"><i class="fa fa-list" style="padding: 5px"> Manage Movies</i></b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/login"><i class="fa fa-sign-in" style="padding: 5px"> Login </i></b-nav-item>
+          <b-nav-item to="/logout"><i class="fa fa-sign-out" style="padding: 5px"> Logout </i></b-nav-item>
+          <i class="fa fa-pied-piper-alt fa-1x" style="padding: 5px; color: white;"></i>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
